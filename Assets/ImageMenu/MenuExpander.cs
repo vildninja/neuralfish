9fee7292c244ae0e5a2be663ef3a89690e8889fe
Jannek <jannek.ek@gmail.com>
2016-04-13 15:30:16 +0200

    Style selector

---
+++ Assets/ImageMenu/MenuExpander.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuExpander : MonoBehaviour
+{
+
+    public GameObject expanded;
+    public GameObject collapsed;
+
+    void Start()
+    {
+        Expand(false);
+    }
+
+    public void Expand(bool expand)
+    {
+        expanded.SetActive(expand);
+        collapsed.SetActive(!expand);
+    }
+}
---
+++ Assets/ImageMenu/TextureSelector.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextureSelector : MonoBehaviour
+{
+
+    public Texture2D target;
+
+    public Texture2D[] styles;
+
+    public void SetStyle(string style)
+    {
+        if (string.IsNullOrEmpty(style))
+        {
+            target.SetPixels(styles[0].GetPixels());
+            target.Apply();
+            return;
+        }
+
+        for (int i = 1; i < styles.Length; i++)
+        {
+            if (styles[i].name.Contains(style))
+            {
+                target.SetPixels(styles[i].GetPixels());
+                target.Apply();
+                return;
+            }
+        }
+    }
+}
